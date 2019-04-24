c29b31660c90c872c57d33995fe52129486efb35
Veena <vebal13@student.sdu.dk>
2019-04-25 00:21:39 +0200

    initial commmit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
+import Route from './route'
+
+const jsx = (
+    <div>
+      <Route />
+    </div>
+    
+)
+    
+
+
+
+ReactDOM.render(jsx, document.getElementById('app'));
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,104 +1,67 @@
 import React, { Component } from "react";
 import Card from 'react-bootstrap/Card';
 
-
-
-
- 
 class Home extends Component {
-
   render() {
-
-   
-
     return (
       <div>
-
-
-
-
-   <br></br>
-
-   
-        <>
+        <br></br>
         <div class="c">
-
-
-  <Card bg="success" text="white" >
-
-
-    <Card.Header>WELCOME</Card.Header>
-    <Card.Body>
-      <Card.Title>GUIDELINES AND STANDARDS
+          <Card bg="success" text="white" >
+            <Card.Header>WELCOME</Card.Header>
+            <Card.Body>
+              <Card.Title>GUIDELINES AND STANDARDS
 FOR INTEGRATED HEALT FACILITY ELECTRONIC MANAGEMENT SYSTEM</Card.Title>
-      <Card.Text>
-      The Government of Tanzania’s (GoT) mission is to facilitate the provision of quality and accessible health services efficiently. Despite the noticeable improvement towards this mission, the sector still faces several challenges. These include inadequate funding, rising health facility management costs, shortage of healthcare workers and inefficiencies in the use of available resources to adequately provide required health services. To overcome these challenges the government has implemented various reforms in the health sector.
-
+              <Card.Text>
+                The Government of Tanzania’s (GoT) mission is to facilitate the provision of quality and accessible health services efficiently. Despite the noticeable improvement towards this mission, the sector still faces several challenges. These include inadequate funding, rising health facility management costs, shortage of healthcare workers and inefficiencies in the use of available resources to adequately provide required health services. To overcome these challenges the government has implemented various reforms in the health sector.
       </Card.Text>
-    </Card.Body>
-  </Card>
-  <br />
-
-
-
-  <Card bg="warning" text="white" >
-    <Card.Body>
-      <Card.Text>
-      Among the reforms, in the health sector, implemented by the government is the introduction of costsharing scheme an attempt to raise additional revenues from health facilities (HSSP III, 2009-2015). To achieve maximum benefit, the government needs to address at least two concerns emanating from this reform. The first concern is to ensure all genuine revenue is collected, recorded and reach relevant authority. To address this concern, the government should put in place implementable mechanisms for ensuring effective revenue collection. It is worth to note that the reform came to replace the old system where majority of health services were free for all and the new cost –sharing scheme came with some fee –waive components for some vulnerable groups. Thus, effective and efficient revenue collection under cost-sharing scheme requires review and re-engineering of Health facility business processes. The second concern is controlling expenditure and service delivery at the health facilities. All of these require re-engineering of Health facility business processes in order to increase net income and reduce the impact of shortage of funds in delivering accessible, efficient and quality health care services.
+            </Card.Body>
+          </Card>
+          <br />
+          <Card bg="warning" text="white" >
+            <Card.Body>
+              <Card.Text>
+                Among the reforms, in the health sector, implemented by the government is the introduction of costsharing scheme an attempt to raise additional revenues from health facilities (HSSP III, 2009-2015). To achieve maximum benefit, the government needs to address at least two concerns emanating from this reform. The first concern is to ensure all genuine revenue is collected, recorded and reach relevant authority. To address this concern, the government should put in place implementable mechanisms for ensuring effective revenue collection. It is worth to note that the reform came to replace the old system where majority of health services were free for all and the new cost –sharing scheme came with some fee –waive components for some vulnerable groups. Thus, effective and efficient revenue collection under cost-sharing scheme requires review and re-engineering of Health facility business processes. The second concern is controlling expenditure and service delivery at the health facilities. All of these require re-engineering of Health facility business processes in order to increase net income and reduce the impact of shortage of funds in delivering accessible, efficient and quality health care services.
       </Card.Text>
-    </Card.Body>
-  </Card>
-  <br />
-
-
-  <Card bg="dark" text="white" >
-    <Card.Body>
-      <Card.Text>
-      Parallel to re-engineering Health facility business processes, automation of well-defined Health facility business process is vital for improving management of scarce resources, reducing workload and increasing productivity. To achieve best results of controlling health resources, the automated system must be able to capture process and disseminate relevant information across all functional units within a health facility and possibly from health facilities to both ministries. This necessitates the need to have an integrated Health Facility electronic Management Systems (iHFeMS) that implements the reengineered Health facility business processes in order to facilitate effective and efficient health resource (include drugs, financial and human capital) management. This is in line with the strategic objective 4 (SO4) of the national eHealth Strategy (2013-2018), which focuses on the use of information technology on financial and resource management.
+            </Card.Body>
+          </Card>
+          <br />
+          <Card bg="dark" text="white" >
+            <Card.Body>
+              <Card.Text>
+                Parallel to re-engineering Health facility business processes, automation of well-defined Health facility business process is vital for improving management of scarce resources, reducing workload and increasing productivity. To achieve best results of controlling health resources, the automated system must be able to capture process and disseminate relevant information across all functional units within a health facility and possibly from health facilities to both ministries. This necessitates the need to have an integrated Health Facility electronic Management Systems (iHFeMS) that implements the reengineered Health facility business processes in order to facilitate effective and efficient health resource (include drugs, financial and human capital) management. This is in line with the strategic objective 4 (SO4) of the national eHealth Strategy (2013-2018), which focuses on the use of information technology on financial and resource management.
       </Card.Text>
-    </Card.Body>
-  </Card>
-  <br />
-
-  <Card bg="primary" text="white" >
-    <Card.Body>
-      <Card.Text>
-      If iHFeMS is well implemented it can improve the quality of Health Facility operations while enhancing revenue collection, management of medicines and other medical supplies. The iHFeMS can also enable patient tracking to increase productivity of inpatient allocation and provide effective administration and control. The iHFeMS supports integration between functions for smooth patient movement within various services. To ensure successful implementation and use of the iHFeMS, a national guideline is fundamental. The iHFeMS implementation guideline for Tanzania aims to provide guidance for development of iHFeMS and set an environment for successful implementation and use of the iHFeMS. It is a directional document that describes minimum requirements, standards and guidelines for successful implementation and use of the iHFeMS in Tanzania’s private and public Health facilities. This document builds on the feasibility study, which was carried out to assess the feasibility of implementing Health facility management system in Tanzanian Health facility setting. It is a product of consistent work carried out by the ICT Department of the Ministry of Health,Community Development, Gender, Elderly and Children with inputs from stakeholders through a participatory process.
+            </Card.Body>
+          </Card>
+          <br />
+          <Card bg="primary" text="white" >
+            <Card.Body>
+              <Card.Text>
+                If iHFeMS is well implemented it can improve the quality of Health Facility operations while enhancing revenue collection, management of medicines and other medical supplies. The iHFeMS can also enable patient tracking to increase productivity of inpatient allocation and provide effective administration and control. The iHFeMS supports integration between functions for smooth patient movement within various services. To ensure successful implementation and use of the iHFeMS, a national guideline is fundamental. The iHFeMS implementation guideline for Tanzania aims to provide guidance for development of iHFeMS and set an environment for successful implementation and use of the iHFeMS. It is a directional document that describes minimum requirements, standards and guidelines for successful implementation and use of the iHFeMS in Tanzania’s private and public Health facilities. This document builds on the feasibility study, which was carried out to assess the feasibility of implementing Health facility management system in Tanzanian Health facility setting. It is a product of consistent work carried out by the ICT Department of the Ministry of Health,Community Development, Gender, Elderly and Children with inputs from stakeholders through a participatory process.
       </Card.Text>
-    </Card.Body>
-  </Card>
-  <br />
-
-  <Card bg="success" text="white" >
-    <Card.Body>
-      <Card.Text>
-      The adoption of this guideline will improve the ongoing reforms in Tanzanian Health facility setting. In addition, this guideline will address some of the key challenges experienced by health facilities implementing Health facility management systems and other related electronic systems that include a lack of proper governance, diverse scope of functionalities, and lack of standards hampering data exchange and information sharing.
-
-
+            </Card.Body>
+          </Card>
+          <br />
+          <Card bg="success" text="white" >
+            <Card.Body>
+              <Card.Text>
+                The adoption of this guideline will improve the ongoing reforms in Tanzanian Health facility setting. In addition, this guideline will address some of the key challenges experienced by health facilities implementing Health facility management systems and other related electronic systems that include a lack of proper governance, diverse scope of functionalities, and lack of standards hampering data exchange and information sharing.
+            </Card.Text>
+            </Card.Body>
+          </Card>
+          <br />
+          <Card bg="warning" text="white" >
+            <Card.Body>
+              <Card.Text>
+                The iHFeMS implementation guideline is applauded as a useful guide to the next steps for Tanzania in its eHealth journey. The document is easy to understand and therefore will help to lead Tanzania towards providing quality healthcare by making management of Health facility more efficient and more effective
       </Card.Text>
-    </Card.Body>
-  </Card>
-  <br />
-
-
-
-  <Card bg="warning" text="white" >
-    <Card.Body>
-      <Card.Text>
-      The iHFeMS implementation guideline is applauded as a useful guide to the next steps for Tanzania in its eHealth journey. The document is easy to understand and therefore will help to lead Tanzania towards providing quality healthcare by making management of Health facility more efficient and more effective
-
-
-      </Card.Text>
-    </Card.Body>
-  </Card>
-  <br />
-
-  </div>
-</>
-
+            </Card.Body>
+          </Card>
+          <br />
+        </div>
       </div>
     );
   }
 }
- 
+
 export default Home;
